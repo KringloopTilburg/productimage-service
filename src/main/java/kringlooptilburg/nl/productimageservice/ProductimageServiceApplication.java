2c75dbee5e73211f5f15cc723d44b9db4629e8de
i476933@fhict.nl <476933@student.fontys.nl>
2024-03-25 18:05:27 +0100

    opzet productimage-service

---
+++ src/main/java/kringlooptilburg/nl/productimageservice/ProductimageServiceApplication.java
@@ -0,0 +1,13 @@
+package kringlooptilburg.nl.productimageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductimageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductimageServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kringlooptilburg/nl/productimageservice/ProductimageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kringlooptilburg.nl.productimageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductimageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

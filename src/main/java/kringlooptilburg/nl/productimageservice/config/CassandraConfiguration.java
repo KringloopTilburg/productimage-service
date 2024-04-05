56f56de16cbc84c77c71397c334449c10dd1dc14
i476933@fhict.nl <476933@student.fontys.nl>
2024-04-05 13:46:05 +0200

    insane imports verwijderd wtfff

--- src/main/java/kringlooptilburg/nl/productimageservice/config/CassandraConfiguration.java
+++ src/main/java/kringlooptilburg/nl/productimageservice/config/CassandraConfiguration.java
@@ -23,4 +23,3 @@ public class CassandraConfiguration extends AbstractCassandraConfiguration {
         return 9042;
     }
 }
-
--- src/main/java/kringlooptilburg/nl/productimageservice/mappers/Mapper.java
+++ src/main/java/kringlooptilburg/nl/productimageservice/mappers/Mapper.java
@@ -1,8 +1,5 @@
 package kringlooptilburg.nl.productimageservice.mappers;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Service;
-
 import java.io.IOException;
 
 public interface Mapper<A,B> {

8fd3abc6b1ee187f94b47a3b2cd1d95a61588079
i476933@fhict.nl <476933@student.fontys.nl>
2024-04-22 16:43:58 +0200

    added replication scalability

--- src/main/java/kringlooptilburg/nl/productimageservice/config/CassandraConfiguration.java
+++ src/main/java/kringlooptilburg/nl/productimageservice/config/CassandraConfiguration.java
@@ -1,25 +1,37 @@
 package kringlooptilburg.nl.productimageservice.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
+import java.net.InetSocketAddress;
+import com.datastax.driver.core.Cluster;
 
 @Configuration
 @EnableCassandraRepositories
 public class CassandraConfiguration extends AbstractCassandraConfiguration {
 
-    @Override
-    public String getContactPoints() {
-        return "127.0.0.1";
-    }
-
+//    @Override
+//    public String getContactPoints() {
+//        return "127.0.0.1";
+//    }
     @Override
     protected String getKeyspaceName() {
         return "productimages";
     }
+//    @Override
+//    protected int getPort() {
+//        return 9042;
+//    }
 
-    @Override
-    protected int getPort() {
-        return 9042;
+    @Bean
+    public Cluster cluster() {
+        return Cluster.builder()
+                .addContactPointsWithPorts(
+                        new InetSocketAddress("127.0.0.1", 9042),
+                        new InetSocketAddress("127.0.0.1", 9043),
+                        new InetSocketAddress("127.0.0.1", 9044)
+                )
+                .build();
     }
 }

32a910f7af67e0c0489d42bbae5a801032f126ac
WimFontys <437045@student.fontys.nl>
2024-06-07 11:13:49 +0200

    Clean-up, Jaeger implementatie, Docker image naar gezamenlijke repo

--- src/test/java/kringlooptilburg/nl/productimageservice/config/TestConfiguration.java
+++ src/test/java/kringlooptilburg/nl/productimageservice/config/TestConfiguration.java
@@ -1,10 +1,14 @@
 package kringlooptilburg.nl.productimageservice.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
+import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
 import kringlooptilburg.nl.productimageservice.repository.PhotoRepository;
 import kringlooptilburg.nl.productimageservice.services.PhotoService;
+import org.mockito.Mockito;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.cassandra.core.CassandraOperations;
 import org.springframework.test.context.TestPropertySource;
@@ -34,5 +38,17 @@ public class TestConfiguration {
     @Bean
     public PhotoRepository photoRepository() {
         return mock(PhotoRepository.class);
+    }
+
+    @Bean
+    @Primary
+    public OtlpGrpcSpanExporter otlpHttpSpanExporter() {
+        return Mockito.mock(OtlpGrpcSpanExporter.class);
+    }
+
+    @Bean
+    @Primary
+    public BatchSpanProcessor batchSpanProcessor() {
+        return Mockito.mock(BatchSpanProcessor.class);
     }
 }
